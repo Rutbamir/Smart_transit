b4ce812a634b0d3abec0024bb4eeab3668b323fa
mohsinreshi <mohsin_reshi@yahoo.com>
2020-07-03 21:05:21 +0530

    minor changes

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -12,54 +12,61 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage('assets/bus.jpg'), fit: BoxFit.cover),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 250.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Container(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/bus.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 250.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
                 child: ButtonTheme(
-              minWidth: 250,
-              height: 50,
-              child: RaisedButton(
-                elevation: 5.0,
-                textColor: Colors.white,
-                color: Colors.orange,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0)),
-                child: Text('Login'),
-                onPressed: () {
-                  Navigator.pushNamed(context, LoginScreen.id);
-                },
+                  minWidth: 250,
+                  height: 50,
+                  child: RaisedButton(
+                    elevation: 5.0,
+                    textColor: Colors.white,
+                    color: Colors.orange,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                    child: Text('Login'),
+                    onPressed: () {
+                      Navigator.pushNamed(context, LoginScreen.id);
+                    },
+                  ),
+                ),
               ),
-            )),
-            SizedBox(
-              height: 25.0,
-            ),
-            Container(
+              SizedBox(
+                height: 25.0,
+              ),
+              Container(
                 child: ButtonTheme(
-              minWidth: 250,
-              height: 50,
-              child: RaisedButton(
-                textColor: Colors.white,
-                color: Colors.orangeAccent[700],
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0)),
-                child: Text('Sign Up'),
-                onPressed: () {
-                  Navigator.pushNamed(context, SignupScreen.id);
-                },
+                  minWidth: 250,
+                  height: 50,
+                  child: RaisedButton(
+                    textColor: Colors.white,
+                    color: Colors.orangeAccent[700],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                    child: Text('Sign Up'),
+                    onPressed: () {
+                      Navigator.pushNamed(context, SignupScreen.id);
+                    },
+                  ),
+                ),
               ),
-            )),
-          ],
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
