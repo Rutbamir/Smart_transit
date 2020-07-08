1f0083c884fd3235544a0959bae36e742a028518
Rutbamir <rutbazehra007@gmail.com>
2020-07-08 18:43:15 +0530

    search bar add

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 const kGoogleApiKey = 'AIzaSyCoxEQwuxZ3LzdaY94LyA_rL85WMVgl5uM';
 
 const kgradientDecoration = BoxDecoration(
-    gradient: LinearGradient(
-  colors: <Color>[Colors.deepOrangeAccent, Colors.orangeAccent],
-));
+  gradient: LinearGradient(
+    colors: <Color>[Colors.deepOrangeAccent, Colors.orangeAccent],
+  ),
+);
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -16,8 +16,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Completer<GoogleMapController> _controller = Completer();
-
-  static const LatLng _center = const LatLng(45.521563, -122.677433);
+  
+  static const LatLng _center = LatLng(34.115829, 74.859138);
 
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
@@ -25,6 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< HEAD
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
@@ -105,5 +106,74 @@ class _HomeScreenState extends State<HomeScreen> {
       print(lat);
       print(lng);
     }
+=======
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          drawer: Drawer(
+            child: ListView(
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  decoration: kgradientDecoration,
+                  accountName: Text('Someone'),
+                  accountEmail: Text('someone@gmail.com'),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: ClipOval(
+                      child: Image.asset('assets/user.jpg'),
+                    ),
+                  ),
+                ),
+                ListTile(
+                    title: Text("My Bookings"),
+                    leading: Icon(Icons.book),
+                    onTap: () {
+                      print('functionality to be added');
+                    }),
+                ListTile(
+                  title: Text("Settings"),
+                  leading: Icon(Icons.settings),
+                ),
+                ListTile(
+                  title: Text("Log Out"),
+                  leading: Icon(Icons.exit_to_app),
+                ),
+              ],
+            ),
+          ),
+          appBar: AppBar(
+            flexibleSpace: Container(decoration: kgradientDecoration),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  //functionality to be added
+                  print('searching');
+                },
+              ),
+            ],
+            backgroundColor: Color.fromRGBO(251, 92, 0, 1),
+          ),
+          body: Column(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  child: GoogleMap(
+                    onMapCreated: _onMapCreated,
+                    initialCameraPosition: CameraPosition(
+                      target: _center,
+                      zoom: 11.0,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+>>>>>>> b4a04dcbe8039ed94c883ccd08184d5d288ded50
   }
 }
