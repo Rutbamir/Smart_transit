ffaec6b3d9c544d3ef23cb0e7e83e32a1cdabf8a
mohsinreshi <mohsin_reshi@yahoo.com>
2020-07-18 02:49:30 +0530

    minor changes to the bottom sheet

--- lib/screens/bottomSheet.dart
+++ lib/screens/bottomSheet.dart
@@ -4,16 +4,61 @@ class AddBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
-      color: Color(0xff757575),
+      height: 180,
       child: Container(
         decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20.0),
-              topRight: Radius.circular(20.0),
-            )),
-        child: Text('hello'),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 10,
+              color: Colors.grey[600],
+              spreadRadius: 5,
+            ),
+          ],
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 18.0,
+            top: 10.0,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                child: Text(
+                  'Icon goes here/new font good?',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.grey[600],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  'Here are your ticket details',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.grey[600],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  'dummy2',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.grey[600],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
