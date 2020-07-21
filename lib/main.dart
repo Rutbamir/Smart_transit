de3567c880ac9397de4dc91a19d31d2c4c1ad430
Rutbamir <rutbazehra007@gmail.com>
2020-07-21 17:09:43 +0530

    add new font

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,24 @@
+import 'package:Smart_transit/animationn.dart';
 import 'package:Smart_transit/screens/ticketScreen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'screens/login_screen.dart';
 import 'screens/signup_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/splash_screen.dart';
 import 'screens/welcome_screen.dart';
+import 'animationn.dart';
 
 void main() => runApp(
       MaterialApp(
         theme: ThemeData(
-          buttonTheme: ButtonThemeData(buttonColor: Colors.lightBlueAccent),
-          primaryColor: Colors.lightBlueAccent,
-          accentColor: Colors.blue[600],
-          fontFamily: 'Product Sans',
-        ),
+            buttonTheme: ButtonThemeData(buttonColor: Colors.lightBlueAccent),
+            primaryColor: Colors.lightBlueAccent,
+            accentColor: Colors.blue[600],
+            textTheme: GoogleFonts.montserratTextTheme()),
         debugShowCheckedModeBanner: false,
-        initialRoute: HomeScreen.id,
+        initialRoute: WelcomeScreen.id,
         routes: {
           LoginScreen.id: (context) => LoginScreen(),
           SignupScreen.id: (context) => SignupScreen(),
@@ -24,6 +26,7 @@ void main() => runApp(
           WelcomeScreen.id: (context) => WelcomeScreen(),
           SplashScreen.id: (context) => SplashScreen(),
           TicketScreen.id: (context) => TicketScreen(),
+          WelcomeAnimation.id: (context) => WelcomeAnimation(),
         },
       ),
     );
