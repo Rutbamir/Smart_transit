4a051f979f374959f93a7541ff24d9a1934e0b74
mohsinreshi <mohsin_reshi@yahoo.com>
2020-07-22 00:12:34 +0530

    add singleChildScrollView to fix overflow | edge case

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -24,59 +24,61 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
       child: Scaffold(
         backgroundColor: Color.fromRGBO(255, 255, 255, 1),
         body: Container(
-            child: Column(
-          children: <Widget>[
-            Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Container(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      "Let's Get Started..",
-                      style: TextStyle(
-                        fontSize: 30.0,
-                      ),
-                    ))),
-            Container(
-              height: 400,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/app.jpg'), fit: BoxFit.contain),
-              ),
-            ),
-            Container(
-              width: 250,
-              height: 50,
-              child: RaisedButton(
-                elevation: 5.0,
-                textColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
+            child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "Let's Get Started..",
+                        style: TextStyle(
+                          fontSize: 30.0,
+                        ),
+                      ))),
+              Container(
+                height: 400,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/app.jpg'), fit: BoxFit.contain),
                 ),
-                child: Text('Login'),
-                onPressed: () {
-                  Navigator.pushNamed(context, LoginScreen.id);
-                },
               ),
-            ),
-            SizedBox(
-              height: 25.0,
-            ),
-            Container(
-              width: 250,
-              height: 50,
-              child: RaisedButton(
-                textColor: Colors.white,
-                color: Colors.blue[600],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
+              Container(
+                width: 250,
+                height: 50,
+                child: RaisedButton(
+                  elevation: 5.0,
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                  child: Text('Login'),
+                  onPressed: () {
+                    Navigator.pushNamed(context, LoginScreen.id);
+                  },
                 ),
-                child: Text('Sign Up'),
-                onPressed: () {
-                  Navigator.pushNamed(context, SignupScreen.id);
-                },
               ),
-            ),
-          ],
+              SizedBox(
+                height: 25.0,
+              ),
+              Container(
+                width: 250,
+                height: 50,
+                child: RaisedButton(
+                  textColor: Colors.white,
+                  color: Colors.blue[600],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                  child: Text('Sign Up'),
+                  onPressed: () {
+                    Navigator.pushNamed(context, SignupScreen.id);
+                  },
+                ),
+              ),
+            ],
+          ),
         )),
       ),
     );
