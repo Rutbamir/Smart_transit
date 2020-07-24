ff76d84a194a42b9cb17e81b20e073d423f59ba9
Rutbamir <rutbazehra007@gmail.com>
2020-07-24 13:27:58 +0530

    add gettickets method

--- lib/models/fetcher.dart
+++ lib/models/fetcher.dart
@@ -1,6 +1,8 @@
+import 'package:Smart_transit/models/auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 Firestore _firestore = Firestore.instance;
+AuthService _auth = AuthService();
 
 Future<List<Map>> getplaces() async {
   List<Map> myplaces = []; //it holds the snapshots
@@ -9,4 +11,10 @@ Future<List<Map>> getplaces() async {
     myplaces.add(place.data);
   }
   return myplaces;
+}
+
+Future<DocumentSnapshot> getTickets() async {
+  String uid = await _auth.getCurrentUser();
+  final tickets = await _firestore.collection('tickets').document('$uid').get();
+  return tickets;
 }
