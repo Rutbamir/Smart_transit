4dae5add698959c49b4179322d0625240b07ec3b
mohsinreshi <mohsin_reshi@yahoo.com>
2020-07-27 01:52:33 +0530

    change color scheme

--- lib/screens/bottomSheet.dart
+++ lib/screens/bottomSheet.dart
@@ -14,86 +14,84 @@ class _AddBottomSheetState extends State<AddBottomSheet> {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 180,
+      height: 200,
       child: Container(
         decoration: BoxDecoration(
           boxShadow: [
             BoxShadow(
-              blurRadius: 10,
+              blurRadius: 4,
               color: Colors.grey[400],
-              spreadRadius: 5,
             ),
           ],
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20.0),
-            topRight: Radius.circular(20.0),
-          ),
+          color: Color(0XFF405289),
+          // Theme.of(context).primaryColor,
         ),
         child: Padding(
           padding: const EdgeInsets.only(
             left: 18.0,
-            top: 10.0,
             bottom: 18.0,
           ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               Expanded(
-                flex: 3,
                 child: SvgPicture.asset(
                   'assets/minus-sign.svg',
                   color: Colors.grey[300],
                 ),
               ),
               SizedBox(
-                height: 10.0,
+                height: 15.0,
               ),
-              Expanded(
-                flex: 3,
-                child: Text(
-                  'Distace: ${GetData.distance.toStringAsFixed(2)} km',
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    color: Colors.grey[600],
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 3,
-                child: Text(
-                  'Cost: Rs ${cost.toStringAsFixed(2)}/-',
-                  style: TextStyle(
-                    fontSize: 25.0,
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 6,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: 250,
-                    child: RaisedButton(
-                      elevation: 2.0,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Center(
                       child: Text(
-                        'Book your ride',
+                        'Distance: ${GetData.distance.toStringAsFixed(2)} km',
                         style: TextStyle(
-                          fontSize: 16.0,
+                          fontSize: 20.0,
                           color: Colors.white,
                         ),
                       ),
-                      onPressed: () {
-                        Navigator.pushNamed(
-                          context,
-                          TicketScreen.id,
-                        );
-                      },
                     ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        '₹ ${cost.toStringAsFixed(2)}/-',
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          color: Color(0XFffff35e),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              Spacer(),
+
+              Expanded(
+                child: Container(
+                  width: 300,
+                  child: RaisedButton(
+                    color: Colors.white,
+                    elevation: 1.0,
+                    child: Text(
+                      'Book your ride',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(
+                        context,
+                        TicketScreen.id,
+                      );
+                    },
                   ),
                 ),
               ),
