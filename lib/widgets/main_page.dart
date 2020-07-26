9f2a5ad93d091bbb5bb4e14265f0df612762a874
mohsinreshi <mohsin_reshi@yahoo.com>
2020-07-27 01:00:38 +0530

    create bottom navbar

--- lib/widgets/main_page.dart
+++ lib/widgets/main_page.dart
@@ -1,6 +1,9 @@
 import 'package:Smart_transit/screens/home_screen.dart';
+import 'package:Smart_transit/screens/login_screen.dart';
+import 'package:Smart_transit/screens/myBookings.dart';
 import 'package:Smart_transit/screens/ticketScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:fluid_bottom_nav_bar/fluid_bottom_nav_bar.dart';
 
 class MainPage extends StatefulWidget {
   static String id = 'main_page';
@@ -9,10 +12,13 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  Widget _child;
   PageController _pageController = PageController();
   List<Widget> _screens = [
     HomeScreen(),
+    LoadTicket(),
     TicketScreen(),
+    LoginScreen(),
   ];
 
   int _selectedIndex = 0;
@@ -24,53 +30,59 @@ class _MainPageState extends State<MainPage> {
 
   void _onItemTapped(int selectedIndex) {
     _pageController.jumpToPage(selectedIndex);
+    _child = AnimatedSwitcher(
+      duration: Duration(
+        milliseconds: 500,
+      ),
+      switchInCurve: Curves.easeOut,
+      switchOutCurve: Curves.easeIn,
+      child: _child,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       body: PageView(
         controller: _pageController,
         children: _screens,
         onPageChanged: _onPageChanged,
         physics: NeverScrollableScrollPhysics(),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: _onItemTapped,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-              color: _selectedIndex == 0
-                  ? Theme.of(context).primaryColor
-                  : Colors.grey,
-            ),
-            title: Text(
-              'Home',
-              style: TextStyle(
-                color: _selectedIndex == 0
-                    ? Theme.of(context).primaryColor
-                    : Colors.grey,
-              ),
-            ),
+      bottomNavigationBar: FluidNavBar(
+        onChange: _onItemTapped,
+        icons: [
+          FluidNavBarIcon(
+            iconPath: 'assets/home.svg',
+            backgroundColor: Colors.white,
+            extras: {'label': 'home'},
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.perm_identity,
-              color: _selectedIndex == 1
-                  ? Theme.of(context).primaryColor
-                  : Colors.grey,
-            ),
-            title: Text(
-              'Profile',
-              style: TextStyle(
-                color: _selectedIndex == 1
-                    ? Theme.of(context).primaryColor
-                    : Colors.grey,
-              ),
-            ),
+          FluidNavBarIcon(
+            iconPath: 'assets/pin.svg',
+            backgroundColor: Colors.white,
+            extras: {'label': 'map'},
+          ),
+          FluidNavBarIcon(
+            iconPath: 'assets/bus.svg',
+            backgroundColor: Colors.white,
+            extras: {'label': 'my bookings'},
+          ),
+          FluidNavBarIcon(
+            iconPath: 'assets/profile.svg',
+            backgroundColor: Colors.white,
+            extras: {'label': 'profile'},
           ),
         ],
+        style: FluidNavBarStyle(
+          iconSelectedForegroundColor: Colors.black,
+          iconUnselectedForegroundColor: Colors.grey[400],
+        ),
+        scaleFactor: 1.5,
+        itemBuilder: (icon, item) => Semantics(
+          label: icon.extras['label'],
+          child: item,
+        ),
       ),
     );
   }
