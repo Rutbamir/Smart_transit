4b9731f521f0f1bbc7d112b19dae9d39a8ff0621
furqanbilal <furquanbazaz@gmail.com>
2020-08-20 19:16:45 +0530

    razorpay integrated, actual ticket cost & redirection after payment yet remaining

--- lib/screens/payment_screen.dart
+++ lib/screens/payment_screen.dart
@@ -1,4 +1,8 @@
+import 'package:Smart_transit/screens/bottomSheet.dart';
 import 'package:flutter/material.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+
+//pwd for razorpay Razorpay@smarttransit1
 
 class PaymentScreen extends StatefulWidget {
   PaymentScreen({Key key}) : super(key: key);
@@ -8,13 +12,85 @@ class PaymentScreen extends StatefulWidget {
 }
 
 class _PaymentScreenState extends State<PaymentScreen> {
+  Razorpay razorpay;
+
+  @override
+  void initState() {
+    super.initState();
+
+    razorpay = new Razorpay();
+
+    razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlerPaymentSuccess);
+    razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlerErrorFailure);
+    razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handlerExtenalWallet);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    razorpay.clear();
+  }
+
+  void openCheckout() {
+    var options = {
+      "key": "rzp_test_KtTxqGSjkN2tvf",
+      "amount": 10000,
+      "name": "Sample App",
+      "description": "Payment for the some random product",
+      "prefill": {"contact": "2323232323", "email": "shdjsdh@gmail.com"},
+      "external": {
+        "wallets": ["paytm"]
+      }
+    };
+
+    try {
+      razorpay.open(options);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void handlerPaymentSuccess() {
+    print("payment success");
+  }
+
+  void handlerErrorFailure() {
+    print("payment Failure");
+  }
+
+  void handlerExtenalWallet() {
+    print("External Wallet");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Payment'),
         centerTitle: true,
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.blue[200],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Amount to be paid $cost'),
+            ),
+            Center(
+              child: RaisedButton(
+                color: Colors.blue,
+                child: Text(
+                  "Pay Now",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  openCheckout();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
